38decb4d5a072342dc269a4d9576202de1dd7082
Luc Rocher <luc.rocher@ens-lyon.fr>
2012-11-25 15:00:59 +0100

    [update] Mise à jour jquery, Twitter Boostrap. Gestion avec Bower.

--- public/assets/grunt.js
+++ public/assets/grunt.js
@@ -1,54 +1,59 @@
-//grunt.loadNpmTasks('grunt-yui-compressor');
-
 module.exports = function(grunt) {
-	grunt.loadNpmTasks('grunt-yui-compressor');
+	grunt.loadNpmTasks('grunt-css');
+	//grunt.loadNpmTasks('grunt-jam');
 
-  // Project configuration.
-  grunt.initConfig({
-    lint: {
-      files: ['grunt.js', 'js/*.js']
-    },
-		
+	// Project configuration.
+	grunt.initConfig({
+
 		min: {
 			dist: {
-				src: ['js/jquery-1.8.1.js', 'js/jquery.dataTables.min.js', 'js/bootstrap.js', 'js/DT_bootstrap.js', 'js/patternizer.min.js', 'js/uiji.js', 'js/script.js'],
+				src: ['components/jquery/jquery.js',
+				'js/jquery.dataTables.min.js',
+				'components/bootstrap/docs/assets/js/bootstrap.js',
+				'js/bootstrap-editable-inline.js',
+				'js/DT_bootstrap.js',
+				'js/patternizer.min.js',
+				'js/uiji.js',
+				'js/script.js'],
 				dest: 'js/main.min.js'
 			}
 		},
 		
 		cssmin: {
 			dist: {
-				src: ['css/bootstrap.css', 'css/nav-fix.css', 'css/style.css'],
+				src: [
+					'components/bootstrap/docs/assets/css/bootstrap.css',
+					'components/bootstrap/docs/assets/css/bootstrap-responsive.css',
+					'css/bootstrap-editable.css',
+					'css/nav-fix.css',
+					'css/style.css'
+				],
 				dest: 'css/main.min.css'
 			}
 		},
-		
-    watch: {
-      files: '<config:lint.files>',
-      tasks: 'lint qunit'
-    },
-    jshint: {
-      options: {
-        curly: true,
-        eqeqeq: true,
-        immed: true,
-        latedef: true,
-        newcap: true,
-        noarg: true,
-        sub: true,
-        undef: true,
-        boss: true,
-        eqnull: true,
-        browser: true
-      },
-      globals: {
-        jQuery: true
-      }
-    },
-    uglify: {}
-  });
 
-  // Default task.
-  grunt.registerTask('default', 'lint concat min');
+		jshint: {
+			options: {
+				curly: true,
+				eqeqeq: true,
+				immed: true,
+				latedef: true,
+				newcap: true,
+				noarg: true,
+				sub: true,
+				undef: true,
+				boss: true,
+				eqnull: true,
+				browser: true
+			},
+			globals: {
+				jQuery: true
+			}
+		},
+		uglify: {}
+	});
+
+	// Default task.
+	grunt.registerTask('default', 'min cssmin');
 
 };
