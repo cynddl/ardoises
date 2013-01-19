d02f9f35815a0d9d9b925d4b96f43bdc70f297b7
Luc Rocher <luc.rocher@ens-lyon.fr>
2013-01-19 16:46:15 +0100

    [fix] Page vide avec message pour les forfaits

---
+++ bundles/rf/views/forfaits.blade.php
@@ -0,0 +1,11 @@
+@layout("rf::home")
+
+@section("rf_content")
+
+{{\Bootstrapper\Breadcrumbs::create(array('RF' => URL::to('rf'), 'Forfaits'))}}
+
+<h2>Gestion des forfaits</h2>
+<p>
+Désolé, cette fonctionnalité n'est pas encore implémentée.
+</p>
+@endsection
