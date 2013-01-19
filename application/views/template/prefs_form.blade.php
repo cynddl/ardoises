95d2f85910e1724e1683b3e409d4239366aa77fe
Luc Rocher <luc.rocher@ens-lyon.fr>
2013-01-19 16:44:45 +0100

    [fix] Pas de modification des préférences par l'utilisateur

--- application/views/template/prefs_form.blade.php
+++ application/views/template/prefs_form.blade.php
@@ -4,25 +4,30 @@
 		<legend>Informations principales</legend>
 		@if(isset($all))
 			{{Former::medium_text('login','Login')}}
+			{{Former::xlarge_text('mail','Adresse mail')}}
+			{{Former::xlarge_text('nom','Nom')}}
+			{{Former::xlarge_text('prenom','Prenom')}}
+			{{Former::medium_password('mdp', 'Mot de passe')}}
 		@else
 			{{Former::medium_uneditable('login','Login')}}	
-		@endif
-		@if(isset($mdp))
-			{{Former::medium_password('mdp', 'Mot de passe')}}
-		@endif
-		{{Former::xlarge_text('mail','Adresse mail')}}
-		{{Former::xlarge_text('nom','Nom')}}
-		{{Former::xlarge_text('prenom','Prenom')}}
+			{{Former::xlarge_uneditable('mail','Adresse mail')}}
+			{{Former::xlarge_uneditable('nom','Nom')}}
+			{{Former::xlarge_uneditable('prenom','Prenom')}}
+		@endif		
 	</fieldset>
 	<fieldset>
 		<legend>Informations complémentaires</legend>
 		@if(isset($all))
-		{{Former::small_number('promo','Promo')}}
-		{{Former::select('departement_id', 'Département')->fromQuery(Departement::all(), 'nom', 'id')}}
+			{{Former::small_number('promo','Promo')}}
+			{{Former::select('departement_id', 'Département')->fromQuery(Departement::all(), 'nom', 'id')}}
 		@else
-		{{Former::small_uneditable('promo','Promo')}}
-		{{Former::xlarge_uneditable('', 'Département')->value($user->departement()->nom)}}
+			{{Former::small_uneditable('promo','Promo')}}
+			{{Former::xlarge_uneditable('', 'Département')->value($user->departement()->nom)}}
 		@endif
 	</fieldset>
-	{{Former::actions(Form::submit('Enregistrer les modifications', array('class'=>'btn btn-primary'))) }}
+	@if(isset($all))
+		{{Former::actions(Form::submit('Enregistrer les modifications', array('class'=>'btn btn-primary'))) }}
+	@else
+		{{Former::actions("N'hésitez pas à contacter un responsable pour une modification de votre profil.")}}
+	@endif
 {{ Former::close() }}
--- bundles/rf/views/ardoises/add.blade.php
+++ bundles/rf/views/ardoises/add.blade.php
@@ -5,5 +5,5 @@
 {{\Bootstrapper\Breadcrumbs::create(array('RF' => URL::to('rf'), 'Ardoises' => URL::to('rf/ardoises'), 'Ajouter un utilisateur'))}}
 
 <h2>Ajouter un utilisateur</h2>
-@render('template.prefs_form', array('all'=>true, 'mdp'=>true, 'user'=>array()))
+@render('template.prefs_form', array('all'=>true, 'user'=>array()))
 @endsection
