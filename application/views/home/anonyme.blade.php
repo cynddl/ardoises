d661434055f1445be8c390408b36ff86f998fa02
Luc Rocher <intnumerix@gmail.com>
2013-03-12 12:15:00 +0100

    [update] Interface anonyme plus ergonomique

--- application/views/home/anonyme.blade.php
+++ application/views/home/anonyme.blade.php
@@ -8,21 +8,26 @@
 		
 		{{HTML::flash()}}
 		
-		<ul class="nav nav-pills">
-		@foreach($groupe as $g)
-		@if($g['nom'] == 'Soft')
-			<li class="active"><a data-pk="{{$g['nom']}}" href="#">{{$g['nom']}} ({{$g['prix']}})</a></li>
-		@else
-			<li><a data-pk="{{$g['nom']}}" href="#">{{$g['nom']}} ({{$g['prix']}})</a></li>
-		@endif
-		@endforeach
-		</ul>
-		
 		{{Form::open()}}
 			<fieldset class="well">
-				{{Former::large_text('conso', 'Consommation', '', true, array('class'=>'typeahead', 'data-provide'=>'typeahead'))}}
+				<h4>Consommation</h4>
+				
+				<select id="conso" name="conso" style="width:50%;">
+					@foreach($groupe as $g)
+						<option value="{{$g['nomreduit']}}">{{$g['nom']}}</option>
+					@endforeach
+			    </select>
+		
+				
 				{{Form::hidden('lieu_id', $lieu->id)}}
 			</fieldset>
+			
+			<ul  class="double">
+			@foreach($groupe as $g)		
+				<li>{{$g['nom']}} <span class="label label-info">{{$g['prix']}}</span></li>
+			@endforeach
+			</ul>
+			
 			<a href="{{URL::to('/')}}" class="btn">Revenir en arrière</a>
 			<button type="submit" class="btn btn-primary pull-right">Débiter</button>
 		{{Form::close()}}
@@ -33,8 +38,25 @@
 
 @section('js')
 <script type="text/javascript" charset="utf-8">
-  var mySource = [@foreach(Groupe::all() as $g)'{{$g->nom}}',@endforeach];
-  $('input.typeahead').typeahead({ 'source': mySource });
+	var mySource = [@foreach(Groupe::all() as $g)'{{$g->nom}}',@endforeach];
+	$('input.typeahead').typeahead({ 'source': mySource });
 	$('li a').click(function(){$('input[name="conso"]').val($(this).attr('data-pk'))});
+	
+	$('#conso').select2();
 </script>
+<style type="text/css" media="screen">
+	.double {
+		margin-bottom: 2em;
+	}
+	.double:after {
+		content: '';
+		display: block;
+		clear: both;
+	}
+	.double li {
+		width: 50%;
+		float:left;
+		display: inline;
+	}
+</style>
 @endsection
